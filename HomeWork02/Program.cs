eedee9804c2826123516ecdc1ee8a81c7f5dd92f
Maxim_Pushkin <psbmm@yandex.ru>
2022-10-19 13:12:45 +0300

    Home Work 02

--- HomeWork02/Program.cs
+++ HomeWork02/Program.cs
@@ -12,27 +12,58 @@ Console.WriteLine($"Вторая цыфра числа: {num} = {result}");
 */
 
 
-//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
+/*
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 int CutNumber( int number)
 {
-    while(number > 100 & number < 1000)
+    while(number >= 1000)
     {
-        number = number / 10;
+        number = number/10;
     }
-    return number;
+return number;
 }
 
-int num = new Random().Next(1000, 10000);
-int result = CutNumber(num);
-int first= result%10;
-Console.WriteLine(num);
-Console.WriteLine(result);
-Console.WriteLine(first);
+int num = new Random().Next(1555, 10000);
 
 if(num < 100)
 {
     Console.WriteLine("Третьей цыфры нет");
+}
+int result = CutNumber(num);
+int first= result%10;
+Console.WriteLine($"случайное число {num}");
+Console.WriteLine($"трех значное число {result}");
+Console.WriteLine($"третье число {first}");
+*/
 
+/*
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+bool Week(int number)
+{
+    if(number == 6 ||number == 7)
+    {
+    return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
+int num = new Random().Next(1, 8);
+bool result = Week(num);
+Console.WriteLine($"День недели: {num}");
+Console.WriteLine(result);
+*/
+
+/*
+Console.Write("Введите число number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+    while(num >= 1000)
+    {
+        num = num/10;
+    }
+ Console.WriteLine(num);
+ */
